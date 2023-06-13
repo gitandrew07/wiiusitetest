155e6fc644da05f468402f9c280baf46a3a9edf3
Andrew <privatoa01@gmail.com>
2023-06-13 13:23:40 +0200

    Initial commit

--- fun.js
+++ fun.js
@@ -6,3 +6,14 @@ function msg() {
 function msg1() {
     alert("wii u")
 }
+function confirmation() {
+    const request = confirm("Are you sure you want to do this? This will use the superskaterhax exploit that is meant to run on 3DS! (dont worry nothing will happen on your Wii U.)")
+
+    if (request) {
+        alert("Press OK to continue")
+        window.location.replace("https://zoogie.github.io/web/super/sk3.html")
+    } else {
+        alert("Press ok to return to the home page.")
+    }
+    
+}
---
+++ launch_site.bat
@@ -0,0 +1,7 @@
+@echo off
+cls
+git add --all
+git commit -m "Initial commit"
+git push origin main
+echo.
+echo Done.
